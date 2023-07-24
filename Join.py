171ea26b23f1da24cdcb5e15666ce8cb4b482a2d
BonitoMinati <134429467+BonitoMinati@users.noreply.github.com>
2023-07-24 23:43:58 +0530

    Add files via upload

---
+++ Join.py
@@ -0,0 +1,41 @@
+def MSG():
+	print(Y)
+	YTC = input("Have U Join Us ? (Y/N): ")
+	if YTC == 'Y' or YTC == 'y':
+		print(G)
+		print("Thank You For Joining Us...\n")
+		time.sleep(4)
+		print("Initializing tool...")
+		time.sleep(4)		
+		print(W + "\n\n")
+		main()
+	elif YTC == 'N' or YTC == 'n':
+		print("")
+		os.system("xdg-open https://bonitominati.in.net/")
+		time.sleep(8)
+		os.system("xdg-open https://github.com/BonitoMinati")
+		time.sleep(3)
+		print(W + "\n\n")
+		main()
+MSG()
+
+def MSG():
+	print(Y)
+	YTC = input("Wanna Join Our Telegram and Whatsapp Group ? (Y/N): ")
+	if YTC == 'N' or YTC == 'N':
+		print(G)
+		print("OH you dont want? Okay...\n")
+		time.sleep(4)
+		print("Initializing tool...")
+		time.sleep(4)		
+		print(W + "\n\n")
+		main()
+	elif YTC == 'Y' or YTC == 'y':
+		print("")
+		os.system("xdg-open https://chat.whatsapp.com/JMYoz1nn90814vHL9AfwVX")
+		time.sleep(8)
+		os.system("xdg-open https://t.me/BonitoMinatiZero")
+		time.sleep(3)
+		print(W + "\n\n")
+		main()
+MSG()
